f8437b0e96188340a58a585bfa58b54aaad2e36e
rouxxi <laskar.gwenvael@gmail.com>
2025-08-13 16:11:51 +0200

    feat: add generation link + selection distant game phase

--- src/pages/GamingRoom.vue
+++ src/pages/GamingRoom.vue
@@ -6,22 +6,31 @@ import GameOverview from '../components/GameOverview.vue';
 import {RequestService} from "../services/request-service.ts";
 import {Subscritpions} from "../services/subscritpions.ts";
 import {userService} from "../services/user-servive.ts";
+import GenerateLinkButton from "../components/GenerateLinkButton.vue";
 
 const route = useRoute();
 const router = useRouter();
-const teams = ref();
-const kings = ref();
+const teamToUpdate = ref();
 const pawnToUpdate = ref();
 const gameInformation = ref();
+const activeTeam = ref()
 // todo prob avec le typing
+
 Subscritpions.subGame(route.params.id, setUpdatedGameInfo);
 Subscritpions.subTeams(route.params.id, setTeamsInfo );
 Subscritpions.subPawns(route.params.id, setPawnsInfo );
 
 const game = ref();
 
-watch([teams,kings, pawnToUpdate, gameInformation ],  async ()=> {
+watch([teamToUpdate, pawnToUpdate, gameInformation ],  async ()=> {
   game.value = await RequestService.get('/games', {id: route.params.id});
+
+  const activeTeamId = game?.value?.active_team;
+  activeTeam.value = game?.value?.teams?.find((team : RawTeam) => team.id === activeTeamId);
+
+  if (game.value?.status === STATUSES.CREATED && game.value?.teams?.every((team) => team.selected)) {
+    await RequestService.put('/games', {id: game.value.id, status: STATUSES.STARTED })
+  }
 })
 
 function setUpdatedGameInfo (payload:GameInformation | unknown) {
@@ -32,59 +41,68 @@ function setPawnsInfo (payload: RawPawn | unknown) {
   pawnToUpdate.value = payload;
 }
 function setTeamsInfo (payload: RawTeam | unknown) {
-  teams.value = payload;
+  teamToUpdate.value = payload;
 }
-
-// function setKingsInfo (payload: RawKing) {
-//   kings.value = payload;
-// }
-const activeTeam = computed(() => {
-  const activeTeamId = game?.value?.active_team;
-  const team = game?.value?.teams?.find((team : RawTeam) => team.id === activeTeamId)
-  return team;
-})
 
 function isInformationLoaded () {
   return game.value?.status
       && game.value?.teams?.length > 0
       && game.value?.pawns?.length > 0
-      && game.value?.kings?.length > 0;
 }
 
 const imInTheGame = computed(() => {
   return game.value?.teams?.find((team: RawTeam)=> team.user_id === userService.me)
 })
 
-onMounted(async ()=> {
-  game.value = await RequestService.get('/games', {id:route.params.id})
-})
-
 async function selectTeam (teamId: number) {
   await RequestService.put('/teams', {id: teamId, user_id: userService.me || null, selected: true})
 }
 
+const linkInvitation = computed(() => {
+  return new URL(route.fullPath, window.location.origin).href;
+})
+
+onMounted(async ()=> {
+  game.value = await RequestService.get('/games', {id:route.params.id})
+
+  const activeTeamId = game?.value?.active_team;
+  activeTeam.value = game?.value?.teams?.find((team : RawTeam) => team.id === activeTeamId);
+})
+
 </script>
 
 <template>
-    <h1> Top Cap duel</h1>
-     <section v-if=" game?.game_mod === 'distant' && !imInTheGame">
-        <h2>Choix de l'équipe</h2>
-       <div v-for="team in game?.teams">
-         <button v-if="!team.selected"  :onclick="()=>selectTeam(team.id)" class="button-choice">
-           <p>{{team.name}}</p>
-           <img class="team-icon" src="/assets/avatars/nut-face.png" alt="computeur image" />
-         </button>
-       </div>
-     </section>
-    <h2 v-if="game?.status === STATUSES.STARTED">C'est à l'équipe {{ game?.active_team }} de jouer</h2>
-  <section >
-    <h2>
-      {{ `C'est à ${activeTeam?.name} de jouer` }}
-    </h2>
-  </section>
-    <GameOverview v-if="(isInformationLoaded() && imInTheGame && game?.game_mod=== 'distant') || (game?.game_mod=== 'local' && isInformationLoaded())" :game="game" :pawnToUpdate/>
 
-    <button :onclick="() => router.go(-1)">back</button>
+  <h1> Top Cap duel</h1>
+  <section v-if="game?.status === STATUSES.CREATED &&  game?.game_mod === 'distant'">
+    <div v-if="!imInTheGame">
+      <h2>Choix de l'équipe</h2>
+      <div v-for="team in game?.teams">
+        <button v-if="!team.selected"  :onclick="()=>selectTeam(team.id)" class="button-choice">
+          <p>{{team.name}}</p>
+          <img class="team-icon" src="/assets/avatars/nut-face.png" alt="computeur image" />
+        </button>
+      </div>
+    </div>
+    <div v-else-if="imInTheGame">
+      <h2> En attente de votre adversaire </h2>
+
+      <GenerateLinkButton :url="linkInvitation" />
+    </div>
+  </section>
+   <section>
+     <h2 v-if="game?.status === STATUSES.STARTED">
+       <span v-if="activeTeam?.user_id === userService.me">
+        {{ `C'est à de jouer` }}
+       </span>
+       <span v-else>
+        {{ `En attente du joueur: ${activeTeam?.name}` }}
+       </span>
+     </h2>
+   </section>
+   <GameOverview v-if="(game?.status === STATUSES.STARTED && (isInformationLoaded() && imInTheGame && game?.game_mod=== 'distant')) || (game?.status === STATUSES.STARTED && (game?.game_mod=== 'local' && isInformationLoaded()))" :game="game" :pawnToUpdate/>
+
+   <button :onclick="() => router.go(-1)">back</button>
 
 </template>
 
